537e973cc651c1a6c71444977b33795d015f0bf8
Princecode <princemwilliams@gmail.com>
2016-06-29 22:57:11 -0400

    Got the first part of the Regex done.

--- Library/Program.cs
+++ Library/Program.cs
@@ -30,7 +30,7 @@ namespace Library
                 //match each needed component
                 //populae with your regex to match the format
 
-                Match match = Regex.Match(s, @"");
+                Match match = Regex.Match(s, @"Type: ([a-zA-z]+),");
                 if (match.Success)
                 {
                     //populate each component with the values from your capture groups
